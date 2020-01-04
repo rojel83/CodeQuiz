6ac0a1bb1eba39a7f6664bcef73be36b47c1048d
Rojel Perez Vez <rojelpv@gmail.com>
2020-01-03 22:58:57 -0500

    fixing grammar

--- assets/Javascript.js
+++ assets/Javascript.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
         answer: "quotes"
     }, {
-        title: "A very useful tool used during debuggingfor printing content to the debugger is:",
+        title: "A very useful tool used during debugging for printing content to the debugger is:",
         choices: ["JavaScript", "terminal/ bash", "for loops", "console.log"],
         answer: "console.log"
     }, ]
